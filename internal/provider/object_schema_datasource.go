50d7740b5b42690c36e413c7f792d693f3cab6ba
Evan Yeung <evan@forevanyeung.com>
2023-10-29 21:04:19 -0400

    object schema datasource

--- internal/provider/object_schema_datasource.go
+++ internal/provider/object_schema_datasource.go
@@ -1,25 +1,174 @@
 package provider
 
 import (
-    "context"
+	// "fmt"
+	"context"
 
-    "github.com/hashicorp/terraform-plugin-framework/datasource"
-    "github.com/hashicorp/terraform-plugin-framework/datasource/schema"
+	"github.com/ctreminiom/go-atlassian/assets"
+	"github.com/hashicorp/terraform-plugin-log/tflog"
+	"github.com/hashicorp/terraform-plugin-framework/datasource"
+	"github.com/hashicorp/terraform-plugin-framework/datasource/schema"
+	"github.com/hashicorp/terraform-plugin-framework/types"
+)
+
+// Ensure the implementation satisfies the expected interfaces.
+var (
+	_ datasource.DataSource              = &objectSchemaDataSource{}
+	_ datasource.DataSourceWithConfigure = &objectSchemaDataSource{}
 )
 
 func NewObjectSchemaDataSource() datasource.DataSource {
-    return &objectSchemaDataSource{}
+	return &objectSchemaDataSource{}
 }
 
-type objectSchemaDataSource struct{}
+// objectSchemaDataSource is the data source implementation.
+type objectSchemaDataSource struct {
+	client *assets.Client
+	worksapce_id string
+}
 
+// Metadata returns the data source type name.
 func (d *objectSchemaDataSource) Metadata(_ context.Context, req datasource.MetadataRequest, resp *datasource.MetadataResponse) {
-    resp.TypeName = req.ProviderTypeName + "_object_schema"
+	resp.TypeName = req.ProviderTypeName + "_object_schema"
 }
 
+// objectSchemaDataSourceModel describes the data source model.
+type objectSchemaDataSourceModel struct {
+	WorkspaceId     types.String `tfsdk:"workspace_id"`
+	GlobalId        types.String `tfsdk:"global_id"`
+	Id              types.String `tfsdk:"id"`
+	Name            types.String `tfsdk:"name"`
+	ObjectSchemaKey types.String `tfsdk:"object_schema_key"`
+	Status          types.String `tfsdk:"status"`
+	Description     types.String `tfsdk:"description"`
+	Created         types.String `tfsdk:"created"`
+	Updated         types.String `tfsdk:"updated"`
+	ObjectCount     types.Int64  `tfsdk:"object_count"`
+	ObjectTypeCount types.Int64  `tfsdk:"object_type_count"`
+	CanManage       types.Bool   `tfsdk:"can_manage"`
+	IdAsInt         types.Int64  `tfsdk:"id_as_int"`
+}
+
+// Schema defines the schema for the data source.
 func (d *objectSchemaDataSource) Schema(_ context.Context, _ datasource.SchemaRequest, resp *datasource.SchemaResponse) {
-    resp.Schema = schema.Schema{}
+	resp.Schema = schema.Schema{
+		Attributes: map[string]schema.Attribute{
+			"workspace_id": schema.StringAttribute{
+				Computed: true,
+			},
+			"global_id": schema.StringAttribute{
+				Computed: true,
+			},
+			"id": schema.StringAttribute{
+				Required: true,
+			},
+			"name": schema.StringAttribute{
+				Computed: true,
+			},
+			"object_schema_key": schema.StringAttribute{
+				Computed: true,
+			},
+			"status": schema.StringAttribute{
+				Computed: true,
+			},
+			"description": schema.StringAttribute{
+				Computed: true,
+			},
+			"created": schema.StringAttribute{
+				Computed: true,
+			},
+			"updated": schema.StringAttribute{
+				Computed: true,
+			},
+			"object_count": schema.Int64Attribute{
+				Computed: true,
+			},
+			"object_type_count": schema.Int64Attribute{
+				Computed: true,
+			},
+			"can_manage": schema.BoolAttribute{
+				Computed: true,
+			},
+			"id_as_int": schema.Int64Attribute{
+				Computed: true,
+			},
+		},
+	}
 }
 
+// Read refreshes the Terraform state with the latest data.
 func (d *objectSchemaDataSource) Read(ctx context.Context, req datasource.ReadRequest, resp *datasource.ReadResponse) {
+	tflog.Debug(ctx, "Reading object schema data source")
+
+	// providerConfig := req.ProviderMeta
+
+	// Create object to hold current state of resource
+	var state objectSchemaDataSourceModel
+	diags := req.Config.Get(ctx, &state)
+	resp.Diagnostics.Append(diags...)
+	if resp.Diagnostics.HasError() {
+		return
+	}
+
+	// Call the API to get the object schema
+	schema, schemaResp, err := d.client.ObjectSchema.Get(ctx, d.worksapce_id, state.Id.ValueString())
+
+	// Return an error if the API call fails
+	if err != nil {
+		resp.Diagnostics.AddError(
+			"Unable to read Assets object schema",
+			err.Error(),
+		)
+		return
+	}
+
+	// Return an error if the HTTP status code is not 200
+	if schemaResp.StatusCode != 200 {
+		resp.Diagnostics.AddError(
+			"Unexpected HTTP status code from Assets API",
+			schemaResp.Status,
+		)
+		return
+	}
+
+	state = objectSchemaDataSourceModel{
+		WorkspaceId:     types.StringValue(schema.WorkspaceId),
+		GlobalId:        types.StringValue(schema.GlobalId),
+		Id:              types.StringValue(schema.Id),
+		Name:            types.StringValue(schema.Name),
+		ObjectSchemaKey: types.StringValue(schema.ObjectSchemaKey),
+		Status:          types.StringValue(schema.Status),
+		Description:     types.StringValue(schema.Description),
+		Created:         types.StringValue(schema.Created),
+		Updated:         types.StringValue(schema.Updated),
+		ObjectCount:     types.Int64Value(int64(schema.ObjectCount)),
+		ObjectTypeCount: types.Int64Value(int64(schema.ObjectTypeCount)),
+		CanManage:       types.BoolValue(schema.CanManage),
+	}
+
+	// Set state
+	diags = resp.State.Set(ctx, &state)
+	resp.Diagnostics.Append(diags...)
+	if resp.Diagnostics.HasError() {
+		return
+	}
+}
+
+func (d *objectSchemaDataSource) Configure(ctx context.Context, req datasource.ConfigureRequest, resp *datasource.ConfigureResponse) {
+	if req.ProviderData == nil {
+		return
+	}
+
+	providerClient := req.ProviderData.(JiraAssetsProviderClient)
+	// client, ok := providerClient.client.(*assets.Client)
+	// if !ok {
+	// 	resp.Diagnostics.AddError(
+	// 		"Unexpected Data Source Configure Type",
+	// 		fmt.Sprintf("Expected *assets.Client, got %T", req.ProviderData),
+	// 	)
+	// 	return
+	// }
+
+	d.client = providerClient.client
+	d.worksapce_id = providerClient.workspaceId
 }
--- internal/provider/provider.go
+++ internal/provider/provider.go
@@ -40,8 +40,14 @@ type JiraAssetsProvider struct {
 // JiraAssetsProviderModel describes the provider data model.
 type JiraAssetsProviderModel struct {
 	WorkspaceId types.String `tfsdk:"workspace_id"`
-	User types.String `tfsdk:"user"`
-	Password types.String `tfsdk:"password"`
+	User        types.String `tfsdk:"user"`
+	Password    types.String `tfsdk:"password"`
+}
+
+// struct describes client and worksapceId
+type JiraAssetsProviderClient struct {
+	client *assets.Client
+	workspaceId string
 }
 
 func (p *JiraAssetsProvider) Metadata(ctx context.Context, req provider.MetadataRequest, resp *provider.MetadataResponse) {
@@ -54,16 +60,16 @@ func (p *JiraAssetsProvider) Schema(ctx context.Context, req provider.SchemaRequ
 		Attributes: map[string]schema.Attribute{
 			"workspace_id": schema.StringAttribute{
 				MarkdownDescription: "Workspace Id of the Asssets instance.",
-				Optional:			true,
+				Optional:            true,
 			},
 			"user": schema.StringAttribute{
 				MarkdownDescription: "Username of an admin or service account with access to the Jira API.",
-				Optional:			true,
+				Optional:            true,
 			},
 			"password": schema.StringAttribute{
 				MarkdownDescription: "Personal access token for the admin or service account.",
-				Optional:			true,
-				Sensitive:			true,
+				Optional:            true,
+				Sensitive:           true,
 			},
 		},
 	}
@@ -81,106 +87,111 @@ func (p *JiraAssetsProvider) Configure(ctx context.Context, req provider.Configu
 	// If practitioner provided a configuration value for any of the attributes, it must be a known value.
 
 	if config.WorkspaceId.IsUnknown() {
-        resp.Diagnostics.AddAttributeError(
-            path.Root("workspaceId"),
-            "Unknown Assets Workspace Id",
-            "The provider cannot create the Assets API client as there is an unknown configuration value for the Assets API workspace Id. " +
-                "Either target apply the source of the value first, set the value statically in the configuration, or use the JIRASSETS_WORKSPACE_ID environment variable.",
-        )
-    }
-
-	if config.User.IsUnknown() {
-        resp.Diagnostics.AddAttributeError(
-            path.Root("user"),
-            "Unknown Assets User",
-            "The provider cannot create the Assets API client as there is an unknown configuration value for the Assets API user. " +
-                "Either target apply the source of the value first, set the value statically in the configuration, or use the JIRASSETS_USER environment variable.",
-        )
-    }
-
-	if config.Password.IsUnknown() {
-        resp.Diagnostics.AddAttributeError(
-            path.Root("password"),
-            "Unknown Assets Password",
-            "The provider cannot create the Assets API client as there is an unknown configuration value for the Assets API password. " +
-                "Either target apply the source of the value first, set the value statically in the configuration, or use the JIRASSETS_PASSWORD environment variable.",
-        )
-    }
-
-	if resp.Diagnostics.HasError() {
-        return
-    }
-
-	// Default values to environment variables, but override with Terraform configuration value if set.
-
-    workspaceId := os.Getenv("JIRASSETS_WORKSPACE_ID")
-    user := os.Getenv("JIRASSETS_USER")
-    password := os.Getenv("JIRASSETS_PASSWORD")
-
-    if !config.WorkspaceId.IsNull() {
-        workspaceId = config.WorkspaceId.ValueString()
-    }
-
-    if !config.User.IsNull() {
-        user = config.User.ValueString()
-    }
-
-    if !config.Password.IsNull() {
-        password = config.Password.ValueString()
-    }
-
-	// If any of the expected configurations are missing, return errors with provider-specific guidance.
-
-    if workspaceId == "" {
-        resp.Diagnostics.AddAttributeError(
-            path.Root("workspaceId"),
-            "Missing Assets API Workspace Id",
-            "The provider cannot create the Assets API client as there is a missing or empty value for the Assets API workspace Id. " +
-                "Set the host value in the configuration or use the JIRASSETS_WORKSPACE_ID environment variable. " +
-                "If either is already set, ensure the value is not empty.",
-        )
-    }
-
-    if user == "" {
-        resp.Diagnostics.AddAttributeError(
-            path.Root("user"),
-            "Missing Assets API User",
-            "The provider cannot create the Assets API client as there is a missing or empty value for the Assets API username. " +
-                "Set the user value in the configuration or use the JIRASSETS_USER environment variable. " +
-                "If either is already set, ensure the value is not empty.",
-        )
-    }
-
-    if password == "" {
-        resp.Diagnostics.AddAttributeError(
-            path.Root("password"),
-            "Missing Assets API Password",
-            "The provider cannot create the Assets API client as there is a missing or empty value for the Assets API password. " +
-                "Set the password value in the configuration or use the JIRASSETS_PASSWORD environment variable. " +
-                "If either is already set, ensure the value is not empty.",
-        )
-    }
-
-    if resp.Diagnostics.HasError() {
-        return
-    }
-
-	// create the Jira Assets client
-	// add authentication headers to the client, workspaceId is added to each request
-
-	client, err := assets.New(nil, "")
-
-	if err != nil {
-		resp.Diagnostics.AddError(
-			"Unable to create Assets client", 
-			"An unexpected error occured wehn creating the Assets API client. Error: " + err.Error(),
+		resp.Diagnostics.AddAttributeError(
+			path.Root("workspaceId"),
+			"Unknown Assets Workspace Id",
+			"The provider cannot create the Assets API client as there is an unknown configuration value for the Assets API workspace Id. "+
+				"Either target apply the source of the value first, set the value statically in the configuration, or use the JIRASSETS_WORKSPACE_ID environment variable.",
 		)
 	}
 
+	if config.User.IsUnknown() {
+		resp.Diagnostics.AddAttributeError(
+			path.Root("user"),
+			"Unknown Assets User",
+			"The provider cannot create the Assets API client as there is an unknown configuration value for the Assets API user. "+
+				"Either target apply the source of the value first, set the value statically in the configuration, or use the JIRASSETS_USER environment variable.",
+		)
+	}
+
+	if config.Password.IsUnknown() {
+		resp.Diagnostics.AddAttributeError(
+			path.Root("password"),
+			"Unknown Assets Password",
+			"The provider cannot create the Assets API client as there is an unknown configuration value for the Assets API password. "+
+				"Either target apply the source of the value first, set the value statically in the configuration, or use the JIRASSETS_PASSWORD environment variable.",
+		)
+	}
+
+	if resp.Diagnostics.HasError() {
+		return
+	}
+
+	// Default values to environment variables, but override with Terraform configuration value if set.
+
+	workspaceId := os.Getenv("JIRASSETS_WORKSPACE_ID")
+	user := os.Getenv("JIRASSETS_USER")
+	password := os.Getenv("JIRASSETS_PASSWORD")
+
+	if !config.WorkspaceId.IsNull() {
+		workspaceId = config.WorkspaceId.ValueString()
+	}
+
+	if !config.User.IsNull() {
+		user = config.User.ValueString()
+	}
+
+	if !config.Password.IsNull() {
+		password = config.Password.ValueString()
+	}
+
+	// If any of the expected configurations are missing, return errors with provider-specific guidance.
+
+	if workspaceId == "" {
+		resp.Diagnostics.AddAttributeError(
+			path.Root("workspaceId"),
+			"Missing Assets API Workspace Id",
+			"The provider cannot create the Assets API client as there is a missing or empty value for the Assets API workspace Id. "+
+				"Set the host value in the configuration or use the JIRASSETS_WORKSPACE_ID environment variable. "+
+				"If either is already set, ensure the value is not empty.",
+		)
+	}
+
+	if user == "" {
+		resp.Diagnostics.AddAttributeError(
+			path.Root("user"),
+			"Missing Assets API User",
+			"The provider cannot create the Assets API client as there is a missing or empty value for the Assets API username. "+
+				"Set the user value in the configuration or use the JIRASSETS_USER environment variable. "+
+				"If either is already set, ensure the value is not empty.",
+		)
+	}
+
+	if password == "" {
+		resp.Diagnostics.AddAttributeError(
+			path.Root("password"),
+			"Missing Assets API Password",
+			"The provider cannot create the Assets API client as there is a missing or empty value for the Assets API password. "+
+				"Set the password value in the configuration or use the JIRASSETS_PASSWORD environment variable. "+
+				"If either is already set, ensure the value is not empty.",
+		)
+	}
+
+	if resp.Diagnostics.HasError() {
+		return
+	}
+
+	// create the Jira Assets client
+	client, err := assets.New(nil, "")
+	
+	if err != nil {
+		resp.Diagnostics.AddError(
+			"Unable to create Assets client",
+			"An unexpected error occured wehn creating the Assets API client. Error: "+err.Error(),
+		)
+	}
+	
+	// add authentication headers to the client, workspaceId is added to each request
 	client.Auth.SetBasicAuth(user, password)
 
-	resp.DataSourceData = client
-	resp.ResourceData = client
+	// add workspaceId to response to be used by resources and data sources
+	providerClient := JiraAssetsProviderClient{
+		client: client,
+		workspaceId: workspaceId,
+	}
+
+	resp.DataSourceData = providerClient
+	resp.ResourceData = providerClient
 }
 
 func (p *JiraAssetsProvider) Resources(ctx context.Context) []func() resource.Resource {
